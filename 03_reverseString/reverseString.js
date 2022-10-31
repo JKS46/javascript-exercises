5e27d72ec4dba6f52e5dc66e5dc976691d4c9d5e
JKS <93701274+JKS46@users.noreply.github.com>
2022-10-31 22:26:43 +0530

    3rd problem done

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    let newWord = "";
+    for(let i = word.length - 1; i >= 0; i--){
+        newWord += word[i];
+    }
+    return newWord;
+    //   return word.split("").reverse().join("");
 };
 
 // Do not edit below this line
