2a41bc8b9513d184c2272569a375ca837204eff3
JKS <93701274+JKS46@users.noreply.github.com>
2022-10-31 22:15:34 +0530

    2nd problem done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(word,num) {
+    let newWord = "";
+    if (num < 0) {
+        return "ERROR";
+    }
+    for(let i = 0; i < num; i++){
+        newWord += word;
+    }
+    return newWord;
+    // return word.repeat(num);
 };
 
 // Do not edit below this line
