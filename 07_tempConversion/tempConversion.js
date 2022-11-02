08dcd240e404cf5a43a3ac3a2b951432a8b2c5b5
Jaswanth Kishore S <93701274+JKS46@users.noreply.github.com>
2022-11-03 00:42:37 +0530

    -

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -6,6 +6,11 @@ const ctof = function() {
 
 };
 
+function round(value, digits) {
+  var rounded = Math.pow(10, digits || 0);
+  return Math.round(value * rounded) / rounded;
+}
+
 // Do not edit below this line
 module.exports = {
   ftoc,
