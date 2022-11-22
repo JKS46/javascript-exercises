6498080db30debcf8d44fe46ff780989dd579a3a
JKS <93701274+JKS46@users.noreply.github.com>
2022-11-22 10:09:21 +0530

    calci done

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -6,20 +6,26 @@ const subtract = function(a,b) {
 	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  if(array[0] == null) return 0;
+	return array.reduce((a,b)=>(a+b));
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((a,b)=>(a*b));
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return Math.pow(a,b);
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	if(a == 0||a==1) return 1;
+  let fact =1;
+  for(i=1;i<=a;i++){
+    fact*=i;
+  }
+  return fact;
 };
 
 // Do not edit below this line
