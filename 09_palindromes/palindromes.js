03a542c19ea74ca1947e0fb7befba5b6e681730d
JKS <93701274+JKS46@users.noreply.github.com>
2022-12-31 04:44:20 +0530

    updated json and finished palindrome

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    let trimmedWord = word.toLowerCase().replace(/[^a-z]/g,"");
+    let newWord = trimmedWord
+                    .split("")
+                    .reverse()
+                    .join("");
+    if(trimmedWord == newWord){
+        return true;
+    }
+    return false;
 };
 
 // Do not edit below this line
