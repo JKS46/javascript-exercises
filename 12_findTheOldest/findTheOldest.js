0e8e8c2bc9c92f200e5802d004711615386b91c4
JKS <93701274+JKS46@users.noreply.github.com>
2022-12-31 06:08:04 +0530

    oldest done

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let oldTimer = people
+                        .sort((a,b)=>
+                        {
+                            if(a.yearOfDeath == undefined){
+                                a.yearOfDeath = (new Date()).getFullYear();
+                            }
+                            if(b.yearOfDeath == undefined){
+                                b.yearOfDeath = (new Date()).getFullYear();
+                            }
+                            if((a.yearOfDeath - a.yearOfBirth)>(b.yearOfDeath - b.yearOfBirth)){
+                                return -1
+                            }
+                            return 1
+                        })
+    console.log(oldTimer[0]);
+    return people[people.indexOf(oldTimer[0])];
 };
 
 // Do not edit below this line
